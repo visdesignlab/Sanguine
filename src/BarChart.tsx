75f0fc2a9d8703edbbbaafc94ad5e46c568604e2
Haihan Lin <haihanlin@HaihandeMacBook.local>
2019-11-19 20:45:30 -0700

    add tooltip

--- src/BarChart.tsx
+++ src/BarChart.tsx
@@ -3,6 +3,7 @@ import * as d3 from "d3";
 import Slider, { Range } from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import { getBoundingClientObj } from 'react-select/src/utils';
+import { any } from 'prop-types';
 
 interface DataPoint {
     x_axis: any,
@@ -13,13 +14,14 @@ interface BarchartState {
     x_axis_name: string,
     data?: DataPoint[],
     y_max?: number,
-    year_range?:number[]
+    year_range?: number[],
+    filter_selection?:string[]
 }
 interface BarchartProps{
     y_axis_name: string,
     x_axis_name: string,
     year_range:number[],
-    filter_selection:String[]
+    filter_selection:string[]
 }
 
 class BarChart extends Component<BarchartState> {
@@ -28,7 +30,8 @@ class BarChart extends Component<BarchartState> {
         x_axis_name: this.props.x_axis_name,
       //  data: [],
         y_max: -1,
-        year_range:[]
+        year_range: [],
+        filter_selection:[]
     } 
     // constructor(props: BarchartProps) {
     //      super(props)
@@ -37,7 +40,27 @@ class BarChart extends Component<BarchartState> {
         console.log(this.props)
         
         let svg= d3.select(".canvas-1").append("svg").attr("width", '100%').attr("height", "100%").attr('id', 'canvas-1-svg');
+        svg
+            .append("g")
+            .attr("id", "x-axis");
+        svg
+            .append("g")
+            .attr("id", "y-axis");
+        svg.append('g').attr('id','all-rects')
         
+        let tooltip = svg.append('g').attr('class', 'tooltip').style('display', 'none')
+        tooltip.append("rect")
+            .attr("width", 30)
+            .attr("height", 20)
+            .attr("fill", "white")
+            .style("opacity", 0.5);
+
+        tooltip.append("text")
+            .attr("x", 15)
+            .attr("dy", "1.2em")
+            .style("text-anchor", "middle")
+            .attr("font-size", "12px")
+            .attr("font-weight", "bold");
      //   this.drawChart(this.props);
     }
     componentWillReceiveProps(nextProps: BarchartProps) {
@@ -54,7 +77,7 @@ class BarChart extends Component<BarchartState> {
      //   this.drawChart(this.props)
     }
     
-    fetch_data_with_year(x_axis: string, y_axis: string,year_min:number,year_max:number, filter_selection:Stirng[]) {
+    fetch_data_with_year(x_axis: string, y_axis: string,year_min:number,year_max:number, filter_selection:string[]) {
         fetch('http://localhost:5000/todo/api/v1.0/requestwithyear', {
             method: 'POST',
             headers: {
@@ -65,7 +88,8 @@ class BarChart extends Component<BarchartState> {
                 x_axis: x_axis,
                 y_axis: y_axis,
                 year_min: year_min.toString(),
-                year_max:year_max.toString()
+                year_max: year_max.toString(),
+                filter_selection:filter_selection.toString()
             })
         })
             .then(res => res.json())
@@ -97,6 +121,7 @@ class BarChart extends Component<BarchartState> {
        // const data = props.data
       //  const y_axis_name = props.y_axis;
        // const x_axis_name = props.x_axis;
+        let that = this;
        console.log(data)
         const x_vals = data.map(function (dp) {
             return dp.x_axis
@@ -105,35 +130,43 @@ class BarChart extends Component<BarchartState> {
         const width = (svg as any).node().getBoundingClientRect().width;
         const height = (svg as any).node().getBoundingClientRect().height;
         const offset = 15
-        svg
-          .append("g")
-          .attr("id", "x-axis")
-          .attr("transform", "translate(0," + (height-offset) + ")");
-        svg
-          .append("g")
-          .attr("id", "y-axis")
-          .attr("transform", "translate(35,-"+offset+")");
+        
+        svg.select('#x-axis').attr("transform", "translate(0," + (height-offset) + ")");
+        
+        svg.select('#y-axis').attr("transform", "translate(35,-" + offset + ")");
+
         const y_max_val = y_max;
         let y_scale = d3.scaleLinear().domain([0, y_max_val]).range([height, offset])
         let x_scale = d3.scaleBand().domain(x_vals).range([35, width]).paddingInner(0.1)
-
-        //const data = this.props.data;
-        console.log(data)
+        const tooltip=svg.select('.tooltip')
+        // //const data = this.props.data;
+        // console.log(data)
         
-        let rects = svg.selectAll("rect")
+        let rects = svg.select('#all-rects').selectAll(".bars")
             .data(data);
+    
         rects.exit()
             .remove();
         rects = (rects as any).enter()
             .append("rect")
             .merge(rects as any);
         rects.attr("x", (d: any) => (x_scale(d.x_axis) as any))
-            .attr("y", (d: any) =>  y_scale(d.y_axis))
+            .attr("y", (d: any) => y_scale(d.y_axis))
+            .classed('bars',true)
             .attr("width", x_scale.bandwidth())
             .attr("height", (d: any) => height -y_scale(d.y_axis))
             .attr("fill", "green")
             .attr('opacity', '0.8')
-            .attr('transform','translate(0,-'+offset+')')
+            .attr('transform', 'translate(0,-' + offset + ')')
+            .on("mouseover", function () { tooltip.style("display", null); })
+            .on("mouseout", function () { tooltip.style("display", "none"); })
+            .on("mousemove", function (d) {
+                
+                var xPosition = d3.mouse(this as any)[0] - 20;
+                var yPosition = d3.mouse(this as any)[1] - 40;
+                tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+                tooltip.select("text").text( d.y_axis);
+            })
         const x_axis = d3.axisBottom(x_scale)
         const y_axis = d3.axisLeft(y_scale)
         svg.select('#x-axis').call(x_axis as any);
