1ae811dfc0ee178897a58854e53145f8e599471b
JackWilb <jackwilburn@tutanota.com>
2020-01-03 17:20:36 -0700

    add some draft tests, update urls

---
+++ backend/api/api/tests.py
@@ -0,0 +1,25 @@
+from django.test import TestCase, Client
+
+class APITestCase(TestCase):
+    def sanity_check(self):
+        self.assertEqual(1, 1)
+
+    def test_get_api_root(self):
+        c = Client()
+        response = c.get("/api/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_attributes(self):
+        c = Client()
+        response = c.get("/api/get_attributes")
+        self.assertEqual(response.status_code, 200)
+
+    def test_summarize_with_year(self):
+        c = Client()
+        response = c.get("/api/summarize_with_year")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_hemoglobin(self):
+        c = Client()
+        response = c.get("/api/hemoglobin")
+        self.assertEqual(response.status_code, 200)
