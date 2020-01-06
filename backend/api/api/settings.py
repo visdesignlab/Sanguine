33e17aae91d6209ab950cc501fb44350f62350e3
JackWilb <jackwilburn@tutanota.com>
2020-01-06 14:27:22 -0700

    add django cors

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
