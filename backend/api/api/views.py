a5a23aa019fb47f1fd3f036acb028f909abbd190
JackWilb97 <jackwilburn@tutanota.com>
2020-04-01 17:49:07 -0700

    change what's returned from execute_sql

--- backend/api/api/views.py
+++ backend/api/api/views.py
@@ -50,8 +50,8 @@ def cpt():
 def execute_sql(command, **kwargs):
     connection = make_connection()
     cur = connection.cursor()
-    result = cur.execute(command, **kwargs)
-    return result
+    return cur.execute(command, **kwargs)
+     
 
 
 def index(request):
