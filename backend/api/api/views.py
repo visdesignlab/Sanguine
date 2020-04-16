d21126452363713382cf181b5e702cf13f23843d
JackWilb97 <jackwilburn@tutanota.com>
2020-04-16 09:03:23 -0700

    move utils to a separate file and start work on a testing results endpoint

--- backend/api/api/views.py
+++ backend/api/api/views.py
@@ -3,82 +3,7 @@ import cx_Oracle
 import csv
 
 from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
-
-
-# Makes and returns the database connection object
-def make_connection():
-    # Load credentials
-    with open("credential.json") as credential:
-        credential_data = json.load(credential)
-        usr_name = credential_data["usr_name"]
-        password = credential_data["password"]
-
-    # Generate the connection
-    dsn_tns = cx_Oracle.makedsn(
-        "prodrac-scan.med.utah.edu",
-        "1521",
-        service_name="dwrac_som_analysts.med.utah.edu",
-    )
-
-    return cx_Oracle.connect(user=usr_name, password=password, dsn=dsn_tns)
-
-
-# Read in the data dictionary
-def data_dictionary():
-    # Instantiate mapping array
-    data_dict = {}
-
-    with open("data_dictionary.csv", "r") as file:
-        read_csv = csv.reader(file, delimiter=",")
-        for row in read_csv:
-            data_dict[row[0]] = row[1]
-
-    return data_dict
-
-
-def cpt():
-    # Instantiate mapping array
-    cpt = []
-    
-    # Read in the cpt codes
-    with open("cpt_codes_cleaned.csv", "r") as file:
-        read_csv = csv.reader(file, delimiter=",")
-        next(read_csv, None)
-        for row in read_csv:
-            cpt.append(tuple(row))
-
-    return cpt
-
-
-# Execute a command against the database
-# *args passes through positional args
-# **kwargs passes through keyword arguments
-def execute_sql(command, *args, **kwargs):
-    connection = make_connection()
-    cur = connection.cursor()
-    return cur.execute(command, *args, **kwargs)
-
-
-# Returns all values from raw results for a specified agg var
-def get_all_by_agg(result_dict, agg, variable):
-    return [
-        y for y in 
-        list(map(lambda x: x[variable] if x["aggregatedBy"] == agg else None, result_dict))
-        if y is not None
-    ]
-
-
-def get_filters(filter_selection):
-    if filter_selection != [""]:
-        filters = filter_selection
-        bindNames = [f":filters{str(i)}" for i in range(len(filters))]
-        filters_safe_sql = f"WHERE CODE_DESC IN ({','.join(bindNames)}) "
-    else:
-        filters = [a[0] for a in cpt()]
-        bindNames = [f":filters{str(i)}" for i in range(len(filters))]
-        filters_safe_sql = f"WHERE CODE IN ({','.join(bindNames)}) "
-
-    return filters, bindNames, filters_safe_sql
+from api.utils import make_connection, data_dictionary, cpt, execute_sql, get_all_by_agg, get_filters
 
 
 def index(request):
@@ -411,6 +336,17 @@ def request_transfused_units(request):
             items = [{"case_id": row[1], "transfused": row[0]}
                  for row in result]
         return JsonResponse({"result": items})
+
+
+def test_results(request):
+    if request.method == "GET":
+        case_ids = request.GET.get("case_ids") or ""
+        test_types = request.GET.get("test_types") or ""
+
+        if not case_ids:
+            HttpResponseBadRequest("You must supply case_ids")
+
+        case_ids = case_ids.split(",")
 
 
 def hemoglobin(request):
