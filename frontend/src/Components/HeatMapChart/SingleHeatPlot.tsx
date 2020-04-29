d02d5f3bfadc3e6ae24c9b5c62d3841018dc1348
Haihan Lin <haihanlin@Haihans-MacBook-Pro.local>
2020-04-28 23:33:21 -0600

    add filter selection on heatmap

--- frontend/src/Components/HeatMapChart/SingleHeatPlot.tsx
+++ frontend/src/Components/HeatMapChart/SingleHeatPlot.tsx
@@ -8,6 +8,7 @@ import { HeatMapDataPoint } from "../../Interfaces/ApplicationState";
 import { ScaleLinear, ScaleOrdinal, ScaleBand, scaleLinear, interpolateReds } from "d3";
 import { highlight_color, basic_gray, blood_red } from "../../ColorProfile";
 import { Popup } from "semantic-ui-react";
+import { actions } from "../..";
 
 interface OwnProps {
     dataPoint: HeatMapDataPoint;
@@ -37,7 +38,18 @@ const SingleHeatPlot: FC<Props> = ({ howToTransform, dataPoint, bandwidth, aggre
                                 fill={output === 0 ? "white" : interpolateReds(colorScale(output / dataPoint.caseCount))}
                                 x={valueScale(point)}
                                 transform={howToTransform}
-                                width={valueScale.bandwidth()} height={bandwidth} />} />,
+                                width={valueScale.bandwidth()}
+                                height={bandwidth}
+                                onClick={(e) => {
+                                    actions.selectSet(
+                                        {
+                                            set_name: aggregatedBy,
+                                            set_value: [dataPoint.aggregateAttribute]
+                                        },
+                                        e.shiftKey
+                                    )
+                                }} />}
+                    />,
                     <line transform={howToTransform} strokeWidth={0.5} stroke={basic_gray} opacity={output === 0 ? 1 : 0} y1={0.5 * bandwidth} y2={0.5 * bandwidth} x1={valueScale(point)! + 0.35 * valueScale.bandwidth()} x2={valueScale(point)! + 0.65 * valueScale.bandwidth()} />]
                 )
             })}
