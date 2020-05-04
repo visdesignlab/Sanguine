c5656ec76676ab8c377c67eea5f2ab021464e2ea
JackWilb97 <jackwilburn@tutanota.com>
2020-05-04 11:43:32 -0700

    don't add agg when all_units

--- backend/api/api/tests.py
+++ backend/api/api/tests.py
@@ -293,12 +293,11 @@ class RequestTransfusedUnitsTestCase(TransactionTestCase):
                 "filter_selection": "REPLACE AORTIC VALVE PERQ FEMORAL ARTRY APPROACH,CORONARY ARTERY BYPASS 1 CORONARY VENOUS GRAFT",
                 "aggregated_by": "YEAR",
             },
-            { # Full example ALL_UNITS
+            { # Full example ALL_UNITS - no agg
                 "transfusion_type": "ALL_UNITS", 
                 "year_range": "2016,2017", 
                 "patient_ids": "68175619,14711172,35383429,632559101",
                 "filter_selection": "REPLACE AORTIC VALVE PERQ FEMORAL ARTRY APPROACH,CORONARY ARTERY BYPASS 1 CORONARY VENOUS GRAFT",
-                "aggregated_by": "YEAR",
             },
         ]
 
