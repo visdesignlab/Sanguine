b012724e57496a9544e1526cd7008892bf34aac1
JackWilb97 <jackwilburn@tutanota.com>
2020-05-05 08:50:20 -0700

    clean up some errors

--- backend/api/api/urls.py
+++ backend/api/api/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path("api/request_individual_specific", views.request_individual_specific, name = "request_individual_specific"),
     path("api/request_fetch_professional_set", views.fetch_professional_set, name = "request_fetch_professional_set"),
     path("api/risk_score", views.risk_score, name = "risk_score"),
-    path("api/patient_outcomes", views.risk_score, name = "patient_outcomes"),
+    path("api/patient_outcomes", views.patient_outcomes, name = "patient_outcomes"),
 ]
