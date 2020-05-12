37577cd381d1d9b86aebb3c78f8f08ba6c2a8b87
JackWilb97 <jackwilburn@tutanota.com>
2020-05-12 13:39:37 -0700

    add basic login form, update settings for login

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "api.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -114,5 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT= os.path.join(BASE_DIR, 'static/')
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+LOGIN_REDIRECT_URL = '/api'
