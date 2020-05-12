a92c2c263652bf7ec481cf73da1825905099483c
JackWilb97 <jackwilburn@tutanota.com>
2020-05-12 13:39:37 -0700

    add basic login form, update settings for login

--- backend/api/api/urls.py
+++ backend/api/api/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path("api/", views.index, name="index"),
     path("api/get_attributes", views.get_attributes, name="get_attributes"),
     path("api/hemoglobin", views.hemoglobin, name = "hemoglobin"),
