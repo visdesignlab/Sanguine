b70bdc3061b81e69ef7e0edd19a58257761884f1
JackWilb97 <jackwilburn@tutanota.com>
2020-06-03 12:13:06 -0600

    remove accidental commit

--- backend/api/api/views.py
+++ backend/api/api/views.py
@@ -222,8 +222,6 @@ def fetch_patient(request):
 
 
 def request_transfused_units(request):
-    return JsonResponse("hello", safe = False)
-
     if request.method == "GET":
         # Get the parameters from the query string
         aggregated_by = request.GET.get("aggregated_by")
@@ -514,7 +512,6 @@ def patient_outcomes(request):
 
 
 def hemoglobin(request):
-    return JsonResponse("hello", safe = False)
     if request.method == "GET":
         command = """
         WITH
