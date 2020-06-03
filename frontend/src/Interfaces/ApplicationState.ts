cea0aec898234856f945764890ef6694ee9ca24e
Haihan Lin <haihanlin@Haihans-MacBook-Pro.local>
2020-06-03 16:13:44 -0600

    add css import in dashboard, add delete function for saved states

--- frontend/src/Interfaces/ApplicationState.ts
+++ frontend/src/Interfaces/ApplicationState.ts
@@ -171,6 +171,25 @@ export const CELL_SAVER_TICKS = ["0", "0-1h", "1h-2h", "2h-3h", "3h-4h", "4h-5h"
 export const presetOptions = [{ value: 1, key: 1, text: "Preset 1" }]
 
 
+export const scatterXOptions = [
+  {
+    value: "PREOP_HEMO",
+    key: "PREOP_HEMO",
+    text: "Preoperative Hemoglobin Value"
+  },
+  {
+    value: "POSTOP_HEMO",
+    key: "POSTOP_HEMO",
+    text: "Postoperative Hemoglobin Value"
+  }
+]
+
+export const typeDiction = ["VIOLIN", "DUMBBELL", "SCATTER", "HEATMAP", "INTERVENTION"]
+
+export const dumbbellValueOptions = [
+  { value: "HEMO_VALUE", key: "HEMO_VALUE", text: "Hemoglobin Value" }
+]
+
 export const dumbbellFacetOptions = [
   { value: "SURGEON_ID", key: "SURGEON_ID", text: "Surgeon ID" },
   { value: "YEAR", key: "YEAR", text: "Year" },
