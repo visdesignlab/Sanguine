268469f66174aa2a148fb583ef5d8708e00085d6
Haihan Lin <haihanlin@Haihans-MacBook-Pro.local>
2020-06-11 14:35:08 -0600

    add route for preview. Preview is similar to dashboard, but uses a different top user control.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import React, { FC, useEffect, useState, Component } from 'react';
+import React, { FC } from 'react';
 import Store from './Interfaces/Store'
 import { inject, observer } from 'mobx-react';
 import Dashboard from './Dashboard';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 
 import Login from './LogIn'
+import Preview from './Preview';
 
 interface OwnProps {
   store?: Store;
@@ -20,12 +21,16 @@ const App: FC<Props> = ({ store }: Props) => {
     <BrowserRouter>
       <Switch>
         {/* <Route exact path='/' component={Home} /> */}
-        <Route exact path='/' component={Login} />
+
         <Route exact path='/dashboard' render={() => {
           // if (isLoggedIn) return <Dashboard />
           // else return <Redirect to="/" />
           return <Dashboard />
         }} />
+        <Route exact path='/preview' render={() => {
+          return <Preview />
+        }} />
+        <Route path='/' component={Login} />
       </Switch></BrowserRouter>
 
     // <Login />
