879be2c53ed0f147fe4d4a2fb445cca99beea3d5
Jack Wilburn <jackwilburn@tutanota.com>
2020-07-21 14:46:36 -0600

    fix issue with not selecting visit no

--- backend/api/api/views.py
+++ backend/api/api/views.py
@@ -466,11 +466,12 @@ def patient_outcomes(request):
         LEFT JOIN (
             SELECT
                 {FIELDS_IN_USE['patient_id']},
+                {FIELDS_IN_USE['visit_no']},
                 CASE WHEN SUM(CASE WHEN CODE IN ('I97.820', '997.02') THEN 1 ELSE 0 END) > 0 THEN 1 ELSE 0 END AS STROKE,
                 CASE WHEN SUM(CASE WHEN CODE IN ('33952', '33954', '33956', '33958', '33962', '33964', '33966', '33973', '33974', '33975', '33976', '33977', '33978', '33979', '33980', '33981', '33982', '33983', '33984', '33986', '33987', '33988', '33989') THEN 1 ELSE 0 END) > 0 THEN 1 ELSE 0 END AS ECMO
             FROM {TABLES_IN_USE['billing_codes']}
             WHERE PRESENT_ON_ADM_F IS NULL
-            GROUP BY {FIELDS_IN_USE['patient_id']}
+            GROUP BY {FIELDS_IN_USE['patient_id']}, {FIELDS_IN_USE['visit_no']}
         ) BLNG_OUTCOMES
             ON VST.{FIELDS_IN_USE['patient_id']} = BLNG_OUTCOMES.{FIELDS_IN_USE['patient_id']} AND VST.{FIELDS_IN_USE['visit_no']} = BLNG_OUTCOMES.{FIELDS_IN_USE['visit_no']}
         LEFT JOIN (
