ca8d35a5d7a12c7b3b8b6deac6307e54dc105cea
Haihan Lin <haihanlin@Haihans-MacBook-Pro.local>
2020-08-12 14:37:12 -0600

    fix a bug on useeffect dependency

--- frontend/src/Components/HeatMapChart/HeatMapVisualization.tsx
+++ frontend/src/Components/HeatMapChart/HeatMapVisualization.tsx
@@ -146,9 +146,7 @@ const BarChartVisualization: FC<Props> = ({ w, notation, hemoglobinDataSet, aggr
                 }
             });
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [proceduresSelection, outcomesSelection, dateRange, showZero, aggregatedBy, valueToVisualize, currentSelectPatientGroupIDs, currentOutputFilterSet
-        //  currentOutputFilterSet
-    ]);
+    }, [proceduresSelection, outcomesSelection, dateRange, showZero, aggregatedBy, valueToVisualize, currentSelectPatientGroupIDs, currentOutputFilterSet, hemoglobinDataSet]);
 
 
 
