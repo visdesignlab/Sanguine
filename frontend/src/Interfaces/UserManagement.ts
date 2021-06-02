b33769aaa8ce98ae6dbabdaa30bbaca3653f7ecb
jackwilb <jackwilburn@tutanota.com>
2021-06-02 15:35:39 -0600

    Fix getCookie by removing whitespace

--- frontend/src/Interfaces/UserManagement.ts
+++ frontend/src/Interfaces/UserManagement.ts
@@ -25,7 +25,7 @@ export function getCookie(name: string) {
     if (document.cookie && document.cookie !== '') {
         var cookies = document.cookie.split(';');
         for (var i = 0; i < cookies.length; i++) {
-            var cookie = cookies[i];
+            var cookie = cookies[i].replace(/\s+/g, '');
             if (cookie.substring(0, name.length + 1) === (name + '=')) {
                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                 break;
