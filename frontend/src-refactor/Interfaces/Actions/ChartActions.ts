42b84c3aee883d8b157af4fa8cdf492111ab6033
Haihan Lin <haihanlin@Haihans-MacBook-Pro.local>
2021-07-06 12:36:27 -0600

    put all actions into different files and written in new trrack format, starting to make store objects.

---
+++ frontend/src-refactor/Interfaces/Actions/ChartActions.ts
@@ -0,0 +1,60 @@
+import { createAction } from "@visdesignlab/trrack";
+import { ActionEvents } from "../Types/EventTypes";
+import { LayoutElement } from "../Types/LayoutTypes";
+import { ApplicationState } from "../Types/StateTypes";
+
+const addExtraPair = createAction<ApplicationState, [string, string]>((state, chartID, newExtraPair) => {
+    state.layoutArray = state.layoutArray.map((d: LayoutElement) => {
+        if (d.i === chartID && d.extraPair) {
+            if (!d.extraPair.includes(newExtraPair)) {
+                let originalArray = JSON.parse(d.extraPair);
+                originalArray.push(newExtraPair);
+                d.extraPair = JSON.stringify(originalArray)
+            }
+        }
+        return d;
+    })
+}).setLabel("addExtraPair")
+
+const removeExtraPair = createAction<ApplicationState, [string, string]>((state, chartID, removingPairName) => {
+    state.layoutArray = state.layoutArray.map((d: LayoutElement) => {
+        if (d.i === chartID && d.extraPair) {
+            let originalArray = JSON.parse(d.extraPair);
+
+            let newArray = (originalArray.filter((l: string) => {
+                return (l !== removingPairName)
+            }))
+            d.extraPair = JSON.stringify(newArray)
+        }
+        return d
+    })
+}).setLabel("removeExtraPair")
+
+const removeChart = createAction<ApplicationState, [string], ActionEvents>((state, indexToRemove) => {
+    state.layoutArray = state.layoutArray.filter(d => d.i !== indexToRemove);
+}).setLabel("removeChart")
+
+const changeNotation = createAction<ApplicationState, [string, string], ActionEvents>((state, chartID, newNotation) => {
+    state.layoutArray = state.layoutArray.map(d => {
+        if (d.i === chartID) {
+            d.notation = newNotation;
+        }
+        return d
+    })
+}).setLabel("changeNotation")
+
+const addNewChart = createAction<ApplicationState, [LayoutElement], ActionEvents>((state: ApplicationState, input) => {
+    state.layoutArray.push(input)
+    state.nextAddingIndex += 1
+}).setLabel("AddChart")
+
+const onLayoutChange = createAction<ApplicationState, [any], ActionEvents>((state, data) => {
+    data.forEach((gridLayout: any) => {
+        let match = state.layoutArray.filter(d => d.i === gridLayout.i)[0]
+        match.w = gridLayout.w;
+        match.h = gridLayout.h;
+        match.x = gridLayout.x;
+        match.y = gridLayout.y;
+    })
+    state.layoutArray = JSON.parse(JSON.stringify(state.layoutArray))
+}).setLabel("LayoutChange")
