a5a50bd35adc0c32b7a11e037a43704c8c589d45
haihan-lin <haihan.lin.7@gmail.com>
2021-07-20 11:59:30 -0600

    change current view style with material-ui:

---
+++ frontend/src/HelperFunctions/DropdownGenerator.ts
@@ -0,0 +1,9 @@
+
+import { DropdownInputTypes } from "../Interfaces/Types/DropdownInputType"
+
+export const DropdownGenerator = (input: DropdownInputTypes[]) => {
+
+    let output = input.map((d) => {
+
+    })
+}
