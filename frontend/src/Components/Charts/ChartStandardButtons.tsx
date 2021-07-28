774ce2f50e321e76ca9a722c8f0f5cdd29053ec6
haihan-lin <haihan.lin.7@gmail.com>
2021-07-28 13:24:18 -0600

    update all chart button icon

--- frontend/src/Components/Charts/ChartStandardButtons.tsx
+++ frontend/src/Components/Charts/ChartStandardButtons.tsx
@@ -1,16 +1,25 @@
+import { IconButton } from "@material-ui/core"
 import { useContext } from "react"
 import { FC } from "react"
-import { Button } from "semantic-ui-react"
+import OpenWithIcon from '@material-ui/icons/OpenWith';
+import CloseIcon from '@material-ui/icons/Close';
+
 import Store from "../../Interfaces/Store"
 type Props = {
     chartID: string
 }
 const ChartStandardButtons: FC<Props> = ({ chartID }: Props) => {
     const store = useContext(Store)
-    return (<>
-        <Button floated="right" icon="close" circular compact size="mini" basic onClick={() => { store.chartStore.removeChart(chartID) }} />
-        <Button floated="right" icon="move" size="mini" circular compact basic className="move-icon" />
-    </>)
+    return (
+        <div style={{ textAlign: "right" }}>
+            <IconButton size="small" className="move-icon">
+                <OpenWithIcon />
+            </IconButton>
+            <IconButton size="small" onClick={() => { store.chartStore.removeChart(chartID) }}>
+                <CloseIcon />
+            </IconButton>
+
+        </div>)
 }
 
 export default ChartStandardButtons
