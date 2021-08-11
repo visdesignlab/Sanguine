7501641c9eb35ec8303bdcd495cfe8fdbbabcc9a
haihan-lin <haihan.lin.7@gmail.com>
2021-08-11 16:30:43 -0600

    allow 1000+ brush

--- frontend/src/Components/Charts/ScatterPlot/ScatterPlot.tsx
+++ frontend/src/Components/Charts/ScatterPlot/ScatterPlot.tsx
@@ -85,7 +85,7 @@ const ScatterPlot: FC<Props> = ({ xAggregationOption, xMax, xMin, yMax, yMin, yV
                 })
 
                 //     !!!!!!!this is the code of checking brushed patient
-                if (caseList.length > 1000 || caseList.length === 0) {
+                if (caseList.length === 0) {
                     updateBrushLoc(null)
                     brushDef.move(svgSelection.select(".brush-layer"), null)
                     store.selectionStore.updateBrush([])
