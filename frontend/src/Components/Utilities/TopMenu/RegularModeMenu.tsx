014cb6297c5b0e5ed391e7180871d1220056920a
haihan-lin <haihan.lin.7@gmail.com>
2021-09-15 15:43:41 -0600

    update figure

--- frontend/src/Components/Utilities/TopMenu/RegularModeMenu.tsx
+++ frontend/src/Components/Utilities/TopMenu/RegularModeMenu.tsx
@@ -8,8 +8,10 @@ import BugReportOutlinedIcon from '@material-ui/icons/BugReportOutlined';
 import { useStyles } from "../../../Presets/StyledComponents";
 import MenuIcon from '@material-ui/icons/Menu';
 import AddModeTopMenu from "./AddModeTopMenu";
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import UndoRedoButtons from "./UndoRedoButtons";
 import StateManagementSuite from "./StateManagementSuite";
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 
 const RegularModeMenu: FC = () => {
     const store = useContext(Store)
@@ -47,23 +49,7 @@ const RegularModeMenu: FC = () => {
                 <MenuIcon />
             </IconButton>
 
-            {/* TODO : This will need to change when merging the branch 
-                All these logos don't look very appealing. Considering having an About page that have all the logos*/}
-            {/* <a href="https://vdl.sci.utah.edu" target="_blank">
-                    <img
-                        className={styles.img}
-                        src="https://raw.githubusercontent.com/visdesignlab/Sanguine/master/images/vdl.png" />
-                </a>
-                <a href="https://vdl.sci.utah.edu" target="_blank">
-                    <img
-                        className={styles.img}
-                        src="https://raw.githubusercontent.com/visdesignlab/Sanguine/UI-updates/images/u-of-u-health-social.png" />
-                </a>
-                <a href="https://vdl.sci.utah.edu" target="_blank">
-                    <img
-                        className={styles.img}
-                        src="https://raw.githubusercontent.com/visdesignlab/Sanguine/UI-updates/images/ARUP-logo.png" />
-                </a> */}
+
             <div className={useStyles().centerAlignment}>
                 <Button variant="contained" onClick={handleClick} aria-controls="simple-menu" aria-haspopup="true" >Add</Button>
                 <Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={() => handleClose()}>
@@ -73,59 +59,56 @@ const RegularModeMenu: FC = () => {
                     <MenuItem onClick={() => handleClose(0)}>Cost and Saving Chart</MenuItem>
                 </Menu>
             </div>
+            <a href="https://healthcare.utah.edu" target="_blank">
+                <img
+                    className={styles.img}
+                    src="https://raw.githubusercontent.com/visdesignlab/Sanguine/UI-updates/images/u-of-u-health-social.png" />
+            </a>
             <Typography className={styles.title} variant="h6" noWrap>
                 Sanguine
             </Typography>
+
+            {/* Preview Mode */}
+            {/* <div className={useStyles().centerAlignment}>
+                <Button variant="outlined" disabled>Preview Mode</Button>
+                //    content="Preview Mode"
+                 // onClick={() => { store!.previewMode = true }} 
+            </div> */}
+
+            {/* TODO : This will need to change when merging the branch 
+                All these logos don't look very appealing. Considering having an About page that have all the logos*/}
+            <a href="https://arup.utah.edu" target="_blank">
+                <img
+                    className={styles.img}
+                    src="https://raw.githubusercontent.com/visdesignlab/Sanguine/UI-updates/images/ARUP-logo.png" />
+            </a>
+            <a href="https://vdl.sci.utah.edu" target="_blank">
+                <img
+                    className={styles.img}
+                    src="https://raw.githubusercontent.com/visdesignlab/Sanguine/master/images/vdl.png" />
+            </a>
+
+
             <StateManagementSuite />
 
+            <UndoRedoButtons />
+
+            <IconButton>
+                <InfoOutlinedIcon />
+            </IconButton>
+
+            <a href="https://github.com/visdesignlab/Sanguine/issues" target="_blank" rel="noopener noreferrer">
+                <IconButton>
+                    <BugReportOutlinedIcon />
+                </IconButton>
+            </a>
+
+            <IconButton onClick={() => { logoutHandler() }} >
+                <ExitToAppIcon />
+            </IconButton>
 
         </Toolbar>
 
-        // <div className={useStyles().root}>
-        //     <Grid container direction="row" justifyContent="space-around" alignItems="center">
-        //        
-
-        //        
-        //         <StateManagementSuite />
-
-        //         {/* Preview Mode */}
-        //         <Divider orientation="vertical" flexItem />
-
-        //         <Grid item xs>
-        //             <div className={useStyles().centerAlignment}>
-        //                 <Button variant="outlined" disabled>Preview Mode</Button>
-        //             </div>
-        //             {/* //    content="Preview Mode"
-        //             // onClick={() => { store!.previewMode = true }}  */}
-
-        //         </Grid>
-        //         <Divider orientation="vertical" flexItem />
-
-        //         <Grid item xs>
-        //             <UndoRedoButtons />
-        //         </Grid>
-
-        //         <Divider orientation="vertical" flexItem />
-
-        //         <Grid item xs>
-        //             <div className={useStyles().centerAlignment}>
-        //                 <Button variant="outlined" onClick={() => { logoutHandler() }} >
-        //                     Log Out
-        //                 </Button>
-        //             </div>
-        //         </Grid>
-
-        //         <Divider orientation="vertical" flexItem />
-
-        //         <Grid item xs>
-        //             <div className={useStyles().centerAlignment}>
-        //                 <a href="https://github.com/visdesignlab/Sanguine/issues" target="_blank" rel="noopener noreferrer">
-        //                     <Button variant="outlined" endIcon={<BugReportOutlinedIcon />}>Report a Bug</Button>
-        //                 </a>
-        //             </div>
-        //         </Grid>
-        //     </Grid>
-        // </div >
     )
 
     const configureOutput = () => {
