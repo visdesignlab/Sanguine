5d6bd5efc164335c61144acbcc7b37838c25ad1c
haihan-lin <haihan.lin.7@gmail.com>
2021-09-22 10:51:33 -0600

    add tooltip for all top menu bar icons.

--- frontend/src/Components/Utilities/TopMenu/UndoRedoButtons.tsx
+++ frontend/src/Components/Utilities/TopMenu/UndoRedoButtons.tsx
@@ -4,19 +4,28 @@ import { FC, useContext } from "react";
 import RedoIcon from '@material-ui/icons/Redo';
 import UndoIcon from '@material-ui/icons/Undo';
 import Store from "../../../Interfaces/Store";
-import { IconButton } from "@material-ui/core";
+import { IconButton, Tooltip } from "@material-ui/core";
+import { useStyles } from "../../../Presets/StyledComponents";
 
 const UndoRedoButtons: FC = () => {
     const store = useContext(Store);
+    const styles = useStyles();
 
-    return <ButtonGroup>
+    return <ButtonGroup size="small">
+
         <IconButton disabled={store.isAtRoot} onClick={() => { store.provenance.undo() }}>
-            <UndoIcon />
+            <Tooltip title={<div>  <p className={styles.tooltipFont}>Undo</p></div>}>
+                <UndoIcon />
+            </Tooltip>
         </IconButton>
+
 
         <IconButton disabled={store.isAtLatest} onClick={() => { store.provenance.redo() }}>
-            <RedoIcon />
+            <Tooltip title={<div>  <p className={styles.tooltipFont}>Redo</p></div>}>
+                <RedoIcon />
+            </Tooltip>
         </IconButton>
+
     </ButtonGroup>
 
 }
