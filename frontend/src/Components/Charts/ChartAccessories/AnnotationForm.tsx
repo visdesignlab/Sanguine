cb2c9103e0fbc8709eeef89d3db0e62679474691
haihan-lin <haihan.lin.7@gmail.com>
2021-09-22 15:34:33 -0600

    auto save notes to browser state onBlur. Also add a success message for note saving.

--- frontend/src/Components/Charts/ChartAccessories/AnnotationForm.tsx
+++ frontend/src/Components/Charts/ChartAccessories/AnnotationForm.tsx
@@ -1,9 +1,9 @@
-import { IconButton, TextField } from "@material-ui/core"
+import { Snackbar, TextField } from "@material-ui/core"
 import { observer } from "mobx-react"
 import { FC, useState, useContext } from "react"
-import SaveIcon from '@material-ui/icons/Save';
 import Store from "../../../Interfaces/Store";
 import { useEffect } from "react";
+
 
 type Props = {
     annotationText: string;
@@ -14,25 +14,31 @@ const AnnotationForm: FC<Props> = ({ annotationText, chartI }: Props) => {
     const [formInput, setFormInput] = useState(annotationText)
     const store = useContext(Store);
 
+
     useEffect(() => {
         setFormInput(annotationText);
     }, [annotationText])
 
     return (<div>
         <TextField
-            style={{ width: "calc(100% - 30px)" }}
+            style={{ width: "100%" }}
             id="outlined-multiline-static"
-            label="Annotation"
+            label="Notes"
             multiline
             size="small"
             value={formInput}
             variant="outlined"
+            onBlur={() => {
+                if (formInput !== annotationText) {
+                    store.chartStore.changeNotation(chartI, formInput);
+                    store.configStore.openNoteSaveSuccessMessage = true;
+                }
+            }}
             onChange={(e) => { setFormInput(e.target.value) }}
         />
-        <IconButton style={{ verticalAlign: "text-top" }} size="small" onClick={() => { store.chartStore.changeNotation(chartI, formInput) }}>
-            <SaveIcon />
-        </IconButton>
-    </div>)
+
+    </div>
+    )
 }
 
 export default observer(AnnotationForm);
