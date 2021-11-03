89ee2c368697be1c79de74406e65dc05542612fb
haihan-lin <haihan.lin.7@gmail.com>
2021-11-03 12:18:27 -0600

    add front end code to process CPT codes

--- frontend/src/Components/Utilities/DetailView/CaseInfo.tsx
+++ frontend/src/Components/Utilities/DetailView/CaseInfo.tsx
@@ -13,7 +13,7 @@ const CaseInfo: FC = () => {
     const [individualInfo, setIndividualInfo] = useState<any>(null);
     const store = useContext(Store);
     const styles = useStyles();
-    const { currentSelectPatient } = store.state
+    const { currentSelectPatient } = store.state;
 
     const generate_List_Items = () => {
         let result = [];
@@ -24,46 +24,48 @@ const CaseInfo: FC = () => {
                         <ListItem>
                             <ListItemText primary={AcronymDictionary[key] ? AcronymDictionary[key] : key} secondary={val as string} />
                         </ListItem>
-                    )
+                    );
                 }
             }
         }
-        return result
-    }
+        return result;
+    };
 
     useEffect(() => {
-        async function fetchIndividualInformaiton() {
+        async function fetchIndividualInformaiton () {
 
             if (currentSelectPatient) {
-                stateUpdateWrapperUseJSON(individualInfo, null, setIndividualInfo)
-                const fetchResult = await fetch(`${process.env.REACT_APP_QUERY_URL}fetch_patient?patient_id=${currentSelectPatient.PATIENT_ID}`)
+                stateUpdateWrapperUseJSON(individualInfo, null, setIndividualInfo);
+                const fetchResult = await fetch(`${process.env.REACT_APP_QUERY_URL}fetch_patient?patient_id=${currentSelectPatient.PATIENT_ID}`);
 
                 const fetchResultJson = await fetchResult.json();
                 const individualInfoJSON = fetchResultJson.result[0];
 
-                const fetchSurgery = await fetch(`${process.env.REACT_APP_QUERY_URL}fetch_surgery?case_id=${currentSelectPatient.CASE_ID}`)
+                const fetchSurgery = await fetch(`${process.env.REACT_APP_QUERY_URL}fetch_surgery?case_id=${currentSelectPatient.CASE_ID}`);
                 const fetchSurgeryJson = await fetchSurgery.json();
-                const surgeryInfo = fetchSurgeryJson.result[0];
+                let surgeryInfo = fetchSurgeryJson.result[0];
+                surgeryInfo["CPT Codes"] = surgeryInfo.cpt.join(', ');
+                delete surgeryInfo.cpt;
 
-                let final_result = Object.assign(individualInfoJSON, surgeryInfo)
+                let final_result = Object.assign(individualInfoJSON, surgeryInfo);
 
-                final_result = Object.assign(final_result, currentSelectPatient)
+                final_result = Object.assign(final_result, currentSelectPatient);
 
-                const outcomeAttributes = ["DEATH", "ECMO", "STROKE", "VENT", "AMICAR", "TXA", "B12"]
+                const outcomeAttributes = ["DEATH", "ECMO", "STROKE", "VENT", "AMICAR", "TXA", "B12"];
                 outcomeAttributes.forEach((attribute) => {
-                    final_result[attribute] = final_result[attribute] === 0 ? "No" : "Yes"
-                })
+                    final_result[attribute] = final_result[attribute] === 0 ? "No" : "Yes";
+                });
 
                 final_result.SURGERY_TYPE = SurgeryUrgencyArray[final_result.SURGERY_TYPE];
-                stateUpdateWrapperUseJSON(individualInfo, final_result, setIndividualInfo)
+                stateUpdateWrapperUseJSON(individualInfo, final_result, setIndividualInfo);
             }
             else {
-                stateUpdateWrapperUseJSON(individualInfo, null, setIndividualInfo)
+                stateUpdateWrapperUseJSON(individualInfo, null, setIndividualInfo);
             }
         }
-        fetchIndividualInformaiton()
+        fetchIndividualInformaiton();
 
-    }, [currentSelectPatient])
+    }, [currentSelectPatient]);
     return (<Container className={styles.containerWidth} style={{ height: "80vh", padding: "1px", visibility: currentSelectPatient ? "visible" : "hidden" }}>
 
         <List dense>
@@ -82,7 +84,7 @@ const CaseInfo: FC = () => {
             </ListSubheader>
             {individualInfo ? generate_List_Items() : <Container className={styles.centerAlignment}><CircularProgress /></Container>}
         </List>
-    </Container>)
-}
+    </Container>);
+};
 
-export default observer(CaseInfo)
+export default observer(CaseInfo);
