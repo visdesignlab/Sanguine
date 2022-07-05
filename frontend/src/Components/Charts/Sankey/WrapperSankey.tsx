15386a159b44c805cea3bd3c7b0ed4753aed131a
haihan-lin <haihan.lin.7@gmail.com>
2022-07-05 17:03:50 -0600

    make sankey working

--- frontend/src/Components/Charts/Sankey/WrapperSankey.tsx
+++ frontend/src/Components/Charts/Sankey/WrapperSankey.tsx
@@ -1,12 +1,16 @@
 import { Chip } from "@material-ui/core";
 import { observer } from "mobx-react";
-import { FC, useCallback, useState } from "react";
+import { FC, useContext, useEffect, useLayoutEffect, useState } from "react";
 import Chart from 'react-google-charts';
 import { DragDropContext, Draggable, DraggableLocation, Droppable, DropResult } from 'react-beautiful-dnd';
 
-import { OutcomeOptions } from "../../../Presets/DataDict";
+import { BloodComponentOptions, OutcomeOptions } from "../../../Presets/DataDict";
 import { DropdownInputTypes } from "../../../Interfaces/Types/DropdownInputType";
 import { useStyles } from "../../../Presets/StyledComponents";
+import { DataContext } from "../../../App";
+import { stateUpdateWrapperUseJSON } from "../../../Interfaces/StateChecker";
+import { BloodProductCap } from "../../../Presets/Constants";
+import { range } from "d3-array";
 
 
 
@@ -18,15 +22,21 @@ type Props = {
 
 const WrapperSankey: FC<Props> = () => {
 
-    const sankeyData = [
-        ['From', 'To', 'Weight'],
-        ['A', 'X', 5],
-        ['A', 'Y', 7],
-        ['A', 'Z', 6],
-        ['B', 'X', 2],
-        ['B', 'Y', 9],
-        ['B', 'Z', 4],
-    ];
+    // const sankeyData = [
+    //     ['From', 'To', 'Weight'],
+    //     ['A', 'X', 5],
+    //     ['A', 'Y', 7],
+    //     ['A', 'Z', 6],
+    //     ['B', 'X', 2],
+    //     ['B', 'Y', 9],
+    //     ['B', 'Z', 4],
+    //     ['X', 'C', 2]
+    // ];
+
+    const styles = useStyles();
+    const hemoData = useContext(DataContext);
+
+    const [sankeyData, updateSankeyData] = useState<(string | number)[][]>([]);
 
     /**
  * Moves an item from one list to another list.
@@ -45,9 +55,27 @@ const WrapperSankey: FC<Props> = () => {
         return result;
     };
 
+    const PRESET = [
+        { value: "B12", key: "B12", text: "B12" },
+        { value: "TXA", key: "TXA", text: "Tranexamic Acid" },
+        { value: "PRBC_UNITS", key: "PRBC_UNITS", text: "Intraoperative RBCs Transfused" },
+        { value: "FFP_UNITS", key: "FFP_UNITS", text: "Intraoperative FFP Transfused" },
+        { value: "VENT", key: "VENT", text: "Ventilator Over 24hr" },
+        { value: "DEATH", key: "DEATH", text: "Death" },
+    ];
+
+    const PRESET_NONSELECTED = [
+        { value: "STROKE", key: "STROKE", text: "Stroke" },
+        { value: "ECMO", key: "ECMO", text: "ECMO" },
+        { value: "AMICAR", key: "AMICAR", text: "Amicar" },
+        { value: "PLT_UNITS", key: "PLT_UNITS", text: "Intraoperative Platelets Transfused" },
+        { value: "CRYO_UNITS", key: "CRYO_UNITS", text: "Intraoperative Cryo Transfused" }
+        // { value: "CELL_SAVER_ML", key: "CELL_SAVER_ML", text: "Cell Salvage Volume (ml)" }
+    ];
+
 
     // with the first element being the attribute selected, and second element not selected
-    const [attributeOptions, setAttributeOptions] = useState([[], OutcomeOptions]);
+    const [attributeOptions, setAttributeOptions] = useState([PRESET, PRESET_NONSELECTED]);
 
     const onDragEnd = (result: DropResult) => {
         const { source, destination } = result;
@@ -68,11 +96,85 @@ const WrapperSankey: FC<Props> = () => {
             newState[sInd] = result[sInd];
             newState[dInd] = result[dInd];
         }
-        setAttributeOptions(newState);
-
-        //TODO next step: assembling data for the sankey
+        stateUpdateWrapperUseJSON(attributeOptions, newState, setAttributeOptions);
     };
-    const styles = useStyles();
+
+
+    useEffect(() => {
+
+        //construct data
+
+        let newSankeyData: (string | number)[][] = [['From', 'To', 'Weight']];
+
+        const sankeyObj: any = {};
+
+        const allOptions = attributeOptions[0].map(({ key }) => {
+            if (!BloodProductCap[key]) {
+                return [key, ['true', 'false']];
+            } else {
+                return [key, range(0, BloodProductCap[key] + 1).map((d, i) => i === BloodProductCap[key] ? `${d}+` : `${d}`)];
+            }
+        });
+
+        allOptions.forEach((attributePair, index) => {
+            if (index < allOptions.length - 1) {
+                const attributeOneOptions = attributePair[1] as string[];
+                const nextAttributeOptions = allOptions[index + 1][1] as string[];
+                attributeOneOptions.forEach((optionOne) => {
+                    sankeyObj[`${attributePair[0]}-${optionOne}`] = {};
+                    nextAttributeOptions.forEach((optionTwo) => {
+                        sankeyObj[`${attributePair[0]}-${optionOne}`][`${allOptions[index + 1][0]}-${optionTwo}`] = 0;
+                        // newSankeyData.push([`${attributePair[0]}-${optionOne}`, `${allOptions[index + 1][0]}-${optionTwo}`, 0]);
+                    });
+                });
+            }
+        });
+
+
+        hemoData.forEach((singleCase) => {
+            attributeOptions[0].forEach(({ key }, index) => {
+                if (index < attributeOptions[0].length - 1) {
+                    let from = '';
+                    let to = '';
+
+                    const nextKey = attributeOptions[0][index + 1]['key'];
+
+                    if (BloodProductCap[key]) {
+                        from = singleCase[key] < BloodProductCap[key] ? `${key}-${singleCase[key]}` : `${key}-${BloodProductCap[key]}+`;
+                    } else {
+                        from = singleCase[key] ? `${key}-true` : `${key}-false`;
+                    }
+                    if (BloodProductCap[nextKey]) {
+                        to = singleCase[nextKey] < BloodProductCap[nextKey] ? `${nextKey}-${singleCase[nextKey]}` : `${nextKey}-${BloodProductCap[nextKey]}+`;
+                    } else {
+                        to = singleCase[nextKey] ? `${nextKey}-true` : `${nextKey}-false`;
+                    }
+                    sankeyObj[from][to] += 1;
+                }
+            });
+        });
+
+        allOptions.forEach(([attributeName, options], index) => {
+            if (index < allOptions.length - 1) {
+                const [nextKey, nextOptions] = allOptions[index + 1];
+
+                (options as string[]).forEach((option) => {
+                    (nextOptions as string[]).forEach((nextOption) => {
+                        const from = `${attributeName}-${option}`;
+                        const to = `${nextKey}-${nextOption}`;
+                        newSankeyData.push([from, to, sankeyObj[from][to]]);
+                    });
+
+                });
+            }
+        });
+
+        //filter out empty entries
+        newSankeyData = newSankeyData.filter(d => d[2] !== 0);
+
+        stateUpdateWrapperUseJSON(sankeyData, newSankeyData, updateSankeyData);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [attributeOptions, hemoData]);
 
 
 
