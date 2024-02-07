ed667c215503fa11e7e826cdc771912762aed0a0
Jack Wilburn <jackwilburn@tutanota.com>
2024-02-07 16:22:35 -0700

    Don't override default settings

--- backend/api/wsgi.py
+++ backend/api/wsgi.py
@@ -7,10 +7,5 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings_prod")
-
 application = get_wsgi_application()
