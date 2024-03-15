85579597a15d707f16f8b2d8e172960fd7a6f4ce
Jack Wilburn <jackwilburn@tutanota.com>
2024-03-15 13:02:38 -0600

    Add some data type coercing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -78,16 +78,48 @@ const App: FC = () => {
             whoamiAPICall(store);
         }
         try {
-            const surgeryCasesFetch = await fetch(`${process.env.REACT_APP_QUERY_URL}get_sanguine_surgery_cases`);
-            const surgeryCases = await surgeryCasesFetch.json();
+            // Fix data types for the surgery cases
+            let minDate = +Infinity;
+            let maxDate = -Infinity;
+            const surgeryCases = { 
+                result: surgeryCasesInput.result.map((d) => {
+                    const preopHemo = parseFloat(`${d.PREOP_HEMO}`);
+                    const postopHemo = parseFloat(`${d.POSTOP_HEMO}`);
+                    const drgWeight = parseFloat(`${d.DRG_WEIGHT}`);
+                    const surgeryTypeIndex = SurgeryUrgencyArray.indexOf(d.SURGERY_TYPE_DESC as any);
+                    const caseDate = new Date(d.CASE_DATE).getTime();
+
+                    minDate = Math.min(minDate, caseDate);
+                    maxDate = Math.max(maxDate, caseDate);
+    
+                    return {
+                        ...d,
+                        CASE_DATE: new Date(d.CASE_DATE).getTime(),
+                        PRBC_UNITS: d.PRBC_UNITS ? d.PRBC_UNITS : 0,
+                        FFP_UNITS: d.FFP_UNITS ? d.FFP_UNITS : 0,
+                        PLT_UNITS: d.PLT_UNITS ? d.PLT_UNITS : 0,
+                        CRYO_UNITS: d.CRYO_UNITS ? d.CRYO_UNITS : 0,
+                        CELL_SAVER_ML: d.CELL_SAVER_ML ? d.CELL_SAVER_ML : 0,
+                        PREOP_HEMO: !Number.isNaN(preopHemo) ? preopHemo : 0,
+                        POSTOP_HEMO: !Number.isNaN(postopHemo) ? postopHemo : 0,
+                        VENT: d.VENT ? d.VENT : 0,
+                        DRG_WEIGHT: !Number.isNaN(drgWeight) ? drgWeight : 0,
+                        DEATH: d.DEATH === "Y" ? 1 : 0,
+                        ECMO: d.ECMO ? d.ECMO : 0,
+                        STROKE: d.STROKE ? d.STROKE : 0,
+                        TXA: d.TXA ? d.TXA : 0,
+                        B12: d.B12 ? d.B12 : 0,
+                        AMICAR: d.AMICAR ? d.AMICAR : 0,
+                        SURGERY_TYPE_DESC: (surgeryTypeIndex > -1 ? SurgeryUrgencyArray[surgeryTypeIndex] : "Unknown") as SurgeryUrgencyType,
+                    };
+                })
+            };
 
             if (surgeryCases.result?.length === 0) {
                 throw new Error("There was an issue fetching data. No results were returned.");
             }
 
-            const nameDictFetch = await fetch(`${process.env.REACT_APP_QUERY_URL}surgeon_anest_names`);
-            const nameDict = await nameDictFetch.json();
-            store.configStore.updateNameDictionary(nameDict);
+            store.state.rawDateRange = [minDate, maxDate];
             setHemoData(surgeryCases.result);
         }
         catch (e) {
