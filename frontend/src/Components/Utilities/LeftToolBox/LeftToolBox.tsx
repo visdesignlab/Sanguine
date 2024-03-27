ea9712ee827cd615e71a27bd195fce4c8974467e
Jack Wilburn <jackwilburn@tutanota.com>
2024-03-27 17:17:19 -0600

    Add a type to the procedure fetch

--- frontend/src/Components/Utilities/LeftToolBox/LeftToolBox.tsx
+++ frontend/src/Components/Utilities/LeftToolBox/LeftToolBox.tsx
@@ -23,7 +23,7 @@ const LeftToolBox: FC = () => {
   useEffect(() => {
     async function fetchData() {
       const procedureFetch = await fetch(`${process.env.REACT_APP_QUERY_URL}get_procedure_counts`);
-      const procedureInput = await procedureFetch.json();
+      const procedureInput = await procedureFetch.json() as { result: { procedureName: string, procedureCodes: string, count: number, overlapList: { [key: string]: number } }[] };
 
       // Process the result into the data type required.
       const result = procedureInput.result.map((procedure: any) => {
