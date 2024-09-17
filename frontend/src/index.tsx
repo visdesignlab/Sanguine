861de9d79727d9436ac020970b7554793e11fcf0
Jack Wilburn <jackwilburn@tutanota.com>
2024-09-17 17:10:56 +0000

    Move CSS to index.html and simplify App.tsx, index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,4 @@
-import CssBaseline from '@mui/material/CssBaseline';
 import ReactDOM from 'react-dom';
-
 import App from './App';
-import './index.css';
-import 'mobx-react-lite/batchingForReactDom';
 
-ReactDOM.render(
-  <>
-    <CssBaseline />
-    <App />
-  </>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
