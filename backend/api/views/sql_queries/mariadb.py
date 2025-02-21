c482045debc51e9b261713e7aff3548963ab8730
Jack Wilburn <jackwilburn@tutanota.com>
2025-02-21 04:03:18 +0000

    Use named bind vars for ids

--- backend/api/views/sql_queries/mariadb.py
+++ backend/api/views/sql_queries/mariadb.py
@@ -88,7 +88,7 @@ patient_query = f"""
         PATIENT.{FIELDS.get('death_date')}
     FROM
         {TABLES.get('patient')} PATIENT
-    WHERE PATIENT.{FIELDS.get('patient_id')} = %s
+    WHERE PATIENT.{FIELDS.get('patient_id')} = %(id)s
     """
 
 surgery_query = f"""
@@ -113,7 +113,7 @@ surgery_query = f"""
                 SURG.{FIELDS.get('prim_proc_desc')},
                 SURG.{FIELDS.get('post_op_icu_los')}
             FROM SANG_SURGERY_CASE SURG
-            WHERE SURG.{FIELDS.get('case_id')} = %s
+            WHERE SURG.{FIELDS.get('case_id')} = %(id)s
         )
     SELECT surg_cases.*, codes.codes AS codes
     FROM surg_cases
