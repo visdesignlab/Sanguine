d08482b42b82c81b1c60c4cdee7f3501a9999492
Luke Schreiber <131496683+Luke-Schreiber@users.noreply.github.com>
2025-03-13 03:14:17 +0000

    Export labels separately from charts

--- frontend/src/Components/Charts/ChartAccessories/ExtraPairPlots/GeneratorExtraPair.tsx
+++ frontend/src/Components/Charts/ChartAccessories/ExtraPairPlots/GeneratorExtraPair.tsx
@@ -7,7 +7,7 @@ import { format, max } from 'd3';
 import { ExtendedExtraPairPoint, ExtraPairPoint } from '../../../../Interfaces/Types/DataTypes';
 import {
   basicGray,
-  ExtraPairPadding, ExtraPairWidth, HGB_HIGH_STANDARD, HGB_LOW_STANDARD, largeFontSize, OffsetDict, regularFontSize,
+  ExtraPairPadding, ExtraPairWidth, HGB_HIGH_STANDARD, HGB_LOW_STANDARD, largeFontSize, regularFontSize,
 } from '../../../../Presets/Constants';
 import ExtraPairViolin from './ExtraPairViolin';
 import ExtraPairBar from './ExtraPairBar';
@@ -15,7 +15,8 @@ import ExtraPairBasic from './ExtraPairBasic';
 import { AcronymDictionary, EXTRA_PAIR_OPTIONS } from '../../../../Presets/DataDict';
 import { BiggerTooltip } from '../../../../Presets/StyledComponents';
 import { BiggerFontProps } from '../../../../Presets/StyledSVGComponents';
-import Store from '../../../../Interfaces/Store';
+import Store, { RootStore } from '../../../../Interfaces/Store';
+import { Offset } from '../../../../Interfaces/Types/OffsetType';
 
 const ExtraPairText = styled('text') <BiggerFontProps>`
   font-size: ${(props) => (props.biggerFont ? `${largeFontSize}px` : `${regularFontSize}px`)};
@@ -41,21 +42,79 @@ interface OwnProps {
     secondaryExtraPairDataSet?: ExtendedExtraPairPoint[];
     aggregationScaleDomain: string;
     aggregationScaleRange: string;
-    text?: boolean;
-    height?: number;
-    chartId: string;
 }
 
 type Props = OwnProps;
 
+// Generates the Attribute Lables & Tooltips
+const extraPairTextGenerator = (
+  store: RootStore,
+  nameInput: typeof EXTRA_PAIR_OPTIONS[number],
+  labelInput: string,
+  type: 'Basic' | 'Violin' | 'BarChart',
+  extraPairDataPoint: ExtraPairPoint,
+  idx: number,
+  allExtraPairs: ExtraPairPoint[],
+  chartId: string,
+  dimensionHeight: number,
+  currentOffset: Offset,
+) => {
+  // Defines the explanation for the tooltip
+  let explanation = '';
+  switch (type) {
+    case 'Basic':
+      explanation = 'Percentage of Patients';
+      break;
+    case 'Violin':
+      explanation = nameInput === 'RISK' ? 'Scaled 0-30' : (`Scaled 0-18, line at ${nameInput as string === 'Preop HGB' ? HGB_HIGH_STANDARD : HGB_LOW_STANDARD}`);
+      break;
+    case 'BarChart':
+      explanation = `Scaled 0-${format('.4r')(max(Object.values(extraPairDataPoint.data)))}`;
+      break;
+    default:
+      break;
+  }
+
+  // Defines the tooltip text
+  const tooltipText = (
+    <BiggerTooltip>
+      {AcronymDictionary[nameInput as keyof typeof AcronymDictionary] ? `${AcronymDictionary[nameInput as keyof typeof AcronymDictionary]}` : undefined}
+      {AcronymDictionary[nameInput as keyof typeof AcronymDictionary] ? <br /> : null}
+      {explanation}
+      {' '}
+      <br />
+      (Click to remove)
+    </BiggerTooltip>
+  );
+
+  // Calculates the x value for the text, based on the width of the previous extra pairs.
+  const labelX: number = allExtraPairs.slice(0, idx + 1).map((extraPair) => ExtraPairWidth[extraPair.type] + ExtraPairPadding).reduce((partialSum, a) => partialSum + a, 0);
+
+  return (
+    <Tooltip title={tooltipText}>
+      <ExtraPairText
+        x={labelX - ExtraPairWidth[type] / 2}
+        y={dimensionHeight - currentOffset.bottom + 25}
+        biggerFont={store.configStore.largeFont}
+        onClick={() => {
+          store.chartStore.removeExtraPair(chartId, nameInput);
+        }}
+      >
+        {labelInput}
+        <RemoveTSpan x={labelX - ExtraPairWidth[type] / 2} dy="-0.5em">x</RemoveTSpan>
+      </ExtraPairText>
+    </Tooltip>
+  );
+};
+
+// Generates the Additional Attributes ("Extra Pair" Plots), without labels.
 function GeneratorExtraPair({
-  extraPairDataSet, secondaryExtraPairDataSet, aggregationScaleDomain, aggregationScaleRange, text = false, height, chartId,
+  extraPairDataSet, secondaryExtraPairDataSet, aggregationScaleDomain, aggregationScaleRange,
 }: Props) {
   let transferedDistance = 0;
   const returningComponents: JSX.Element[] = [];
-  const currentOffset = OffsetDict.regular;
-  const store = useContext(Store);
 
+  // For each extra pair, generate the appropriate plot.
   extraPairDataSet.forEach((pairData, idx) => {
     let temporarySecondary = secondaryExtraPairDataSet;
     if (secondaryExtraPairDataSet) {
@@ -115,65 +174,42 @@ function GeneratorExtraPair({
         break;
     }
   });
-  const extraPairTextGenerator = (
-    nameInput: typeof EXTRA_PAIR_OPTIONS[number],
-    labelInput: string,
-    type: 'Basic' | 'Violin' | 'BarChart',
-    extraPairDataPoint: ExtraPairPoint,
-    idx: number,
-    allExtraPairs: ExtraPairPoint[],
-    dimensionHeight: number,
-  ) => {
-    let explanation = '';
-    switch (type) {
-      case 'Basic':
-        explanation = 'Percentage of Patients';
-        break;
-      case 'Violin':
-        explanation = nameInput === 'RISK' ? 'Scaled 0-30' : (`Scaled 0-18, line at ${nameInput as string === 'Preop HGB' ? HGB_HIGH_STANDARD : HGB_LOW_STANDARD}`);
-        break;
-      case 'BarChart':
-        explanation = `Scaled 0-${format('.4r')(max(Object.values(extraPairDataPoint.data)))}`;
-        break;
-      default:
-        break;
-    }
-
-    const tooltipText = (
-      <BiggerTooltip>
-        {AcronymDictionary[nameInput as keyof typeof AcronymDictionary] ? `${AcronymDictionary[nameInput as keyof typeof AcronymDictionary]}` : undefined}
-        {AcronymDictionary[nameInput as keyof typeof AcronymDictionary] ? <br /> : null}
-        {explanation}
-        {' '}
-        <br />
-        (Click to remove)
-      </BiggerTooltip>
-    );
-
-    const xPadding: number = allExtraPairs.slice(0, idx + 1).map((extraPair) => ExtraPairWidth[extraPair.type] + ExtraPairPadding).reduce((partialSum, a) => partialSum + a, 0);
-
-    return (
-      <Tooltip title={tooltipText}>
-        <ExtraPairText
-          x={xPadding - ExtraPairWidth[type] / 2}
-          y={dimensionHeight - currentOffset.bottom + 25}
-          biggerFont={store.configStore.largeFont}
-          onClick={() => {
-            store.chartStore.removeExtraPair(chartId, nameInput);
-          }}
-        >
-          {labelInput}
-          <RemoveTSpan x={xPadding - ExtraPairWidth[type] / 2} dy="-0.5em">x</RemoveTSpan>
-        </ExtraPairText>
-      </Tooltip>
-    );
-  };
   return (
     <g>
       {returningComponents}
-      {text && height ? extraPairDataSet.map((extraPairDataPoint, idx) => extraPairTextGenerator(extraPairDataPoint.name, extraPairDataPoint.label, extraPairDataPoint.type, extraPairDataPoint, idx, extraPairDataSet, height)) : null}
     </g>
   );
 }
 
 export default observer(GeneratorExtraPair);
+
+// Generates the Attribute Lables & Tooltips, separate from the charts.
+export function ExtraPairLabels({
+  extraPairDataSet,
+  chartId,
+  dimensionHeight,
+  currentOffset,
+}: {
+  extraPairDataSet: ExtraPairPoint[];
+  chartId: string;
+  dimensionHeight: number;
+  currentOffset: Offset;
+}): JSX.Element {
+  const store = useContext(Store);
+  return (
+    <>
+      {extraPairDataSet.map((extraPairDataPoint, idx) => extraPairTextGenerator(
+        store,
+        extraPairDataPoint.name,
+        extraPairDataPoint.label,
+        extraPairDataPoint.type,
+        extraPairDataPoint,
+        idx,
+        extraPairDataSet,
+        chartId,
+        dimensionHeight,
+        currentOffset,
+      ))}
+    </>
+  );
+}
