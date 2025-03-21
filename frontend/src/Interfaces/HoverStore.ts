a1a6ffd3742dd57f1a5b4811fae93c4fef83ecef
Luke Schreiber <131496683+Luke-Schreiber@users.noreply.github.com>
2025-03-21 21:25:08 +0000

    Create a hover store

---
+++ frontend/src/Interfaces/HoverStore.ts
@@ -0,0 +1,49 @@
+import { makeAutoObservable } from 'mobx';
+// eslint-disable-next-line import/no-cycle
+import { RootStore } from './Store';
+
+/**
+ * @typedef {Object} HoverStore
+ * @property {number[]} hoveredCaseIds - The list of hovered case IDs
+ * @property {string} hoverColor - The color of the hover
+ * @property {RootStore} rootStore - The root store
+ */
+export class HoverStore {
+  rootStore: RootStore;
+
+  // Currently hovered case IDs
+  private _hoveredCaseIds: number[];
+
+  // Color of the hover
+  public readonly hoverColor: string;
+
+  // Extends the root store
+  constructor(rootStore: RootStore) {
+    this.rootStore = rootStore;
+
+    // Currently hovered case IDs
+    this._hoveredCaseIds = [];
+
+    // Color of the hover
+    this.hoverColor = '#FFE8BE';
+
+    // Make the store observable
+    makeAutoObservable(this);
+  }
+
+  /**
+   * Getter for the hoveredCaseIds
+   * @returns {number[]} The list of hovered case IDs
+   */
+  get hoveredCaseIds() {
+    return this._hoveredCaseIds;
+  }
+
+  /**
+   * Setter for the hoveredCaseIds
+   * @param {number[]} ids - The list of hovered case IDs
+   */
+  set hoveredCaseIds(ids: number[]) {
+    this._hoveredCaseIds = ids;
+  }
+}
