dc13bbf5176f276a4d5b1e90fa6314f82a5f4f0f
Luke-Schreiber <131496683+Luke-Schreiber@users.noreply.github.com>
2025-04-17 20:35:57 +0000

    Multiple cases appear

--- frontend/src/Components/Charts/DumbbellChart/WrapperDumbbell.tsx
+++ frontend/src/Components/Charts/DumbbellChart/WrapperDumbbell.tsx
@@ -98,7 +98,7 @@ function WrapperDumbbell({ layout }: { layout: DumbbellLayoutElement }) {
       const postopHgb = ob.POSTOP_HEMO;
       let yAxisVal;
       yAxisVal = ob[xAxisVar] as number;
-      if (yAxisVal !== undefined && preopHgb > 0 && postopHgb > 0 && !existingCaseID.has(ob.CASE_ID)) {
+      if (yAxisVal !== undefined && preopHgb > 0 && postopHgb > 0 && !existingCaseID.has(ob.case_id)) {
         if ((showZero) || (!showZero && yAxisVal > 0)) {
           yAxisVal = bloodComponentOutlierHandler(yAxisVal, xAxisVar);
           tempXMin = preopHgb < tempXMin ? preopHgb : tempXMin;
