fab4fcb7c119ca7aff888f6ee9cecbb6b3eb83e6
Jack Wilburn <jackwilburn@tutanota.com>
2025-05-15 14:06:39 -0600

    changes to make oracle work

--- backend/api/views/sql_queries/__init__.py
+++ backend/api/views/sql_queries/__init__.py
@@ -1,2 +1,2 @@
-from .mariadb import procedure_count_query, patient_query, surgery_query, surgery_case_query
+from .oracle import procedure_count_query, patient_query, surgery_query, surgery_case_query
 all = [procedure_count_query, patient_query, surgery_query, surgery_case_query]
