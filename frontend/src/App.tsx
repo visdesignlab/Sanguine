0f94d8ac7bead5ed7f433dbab67b39d92ba5a73e
Luke-Schreiber <131496683+Luke-Schreiber@users.noreply.github.com>
2025-07-17 17:43:11 +0000

    App - comments, update dependency array

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,11 +11,12 @@ import BrowserWarning from './Components/Modals/BrowserWarning';
 import DataRetrieval from './Components/Modals/DataRetrieval';
 
 function App() {
+  // Data Loading states
   const store = useContext(Store);
-
   const [dataLoading, setDataLoading] = useState(true);
   const [dataLoadingFailed, setDataLoadingFailed] = useState(false);
 
+  // Idle timer to log out user after 30 minutes of inactivity
   useIdleTimer({
     timeout: 1000 * 60 * 30, // 1000 ms * 60 s * 30 min
     onIdle: () => logoutHandler(),
@@ -24,32 +25,38 @@ function App() {
     throttle: 1000 * 60,
   });
 
+  // Fetch all visits data on initial load
   useEffect(() => {
-    async function fetchAllPatients() {
+    async function fetchAllVisits() {
       try {
+        // Fetch all visits data
         const visitsRequest = await fetch(`${import.meta.env.VITE_QUERY_URL}get_all_data`);
         const visits = await visitsRequest.json() as Visit[];
 
+        // Check if visits data is empty
         if (visits.length === 0) {
           throw new Error('There was an issue fetching data. No results were returned.');
         }
 
+        // Update the store with fetched visits
         store.allVisits = visits;
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
       } catch (e) {
         setDataLoadingFailed(true);
       } finally {
+        // Data loading is complete
         setDataLoading(false);
       }
     }
-
-    fetchAllPatients();
-  }, []);
+    // Call the function to fetch visits data
+    fetchAllVisits();
+  }, [store]);
 
   return (
     <MantineProvider theme={mantineTheme}>
       <Shell />
       <>
+        { /* Data loading modal */}
         <BrowserWarning />
         <DataRetrieval dataLoading={dataLoading} dataLoadingFailed={dataLoadingFailed} />
       </>
