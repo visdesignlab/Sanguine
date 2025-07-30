e4cf7a35ad4c2cbc4bdb368ca22a3ac9f323daf5
Luke-Schreiber <131496683+Luke-Schreiber@users.noreply.github.com>
2025-07-30 03:02:44 +0000

    Refactor mantine theme

--- frontend/src/Theme/mantineTheme.ts
+++ frontend/src/Theme/mantineTheme.ts
@@ -1,60 +1,60 @@
 import { createTheme, useMantineTheme, px } from '@mantine/core';
 
-// Icon border colors and styles ------------------
-const BORDER_COLOR = 'var(--mantine-color-black)';
-const BORDER_STYLE = {
-  borderWidth: '1px',
+// Standard text sizes across application ----------------
+const standardTextSizes = {
+  h1: {
+    fontWeight: '600',
+    fontSize: '1.5rem',
+  },
+  h2: {
+    fontWeight: '600',
+    fontSize: '1.25rem',
+  },
+  h3: {
+    fontWeight: '600',
+    fontSize: '1.125rem',
+  },
+  h4: {
+    fontWeight: '400',
+    fontSize: '1rem',
+  },
+};
+
+// Application stroke styles ------------------
+// Stroke color
+const BLACK_STROKE_COLOR = 'var(--mantine-color-black)';
+const HIGHLIGHT_STROKE_COLOR = 'var(--mantine-color-blue-6)';
+// Stroke widths
+const STROKE_WIDTH = '1px';
+const DIMMED_STROKE_WIDTH = '1.5px';
+
+// Tooltip styles --------------
+const TOOLTIP_STYLES = {
+  borderWidth: STROKE_WIDTH,
   borderStyle: 'solid' as const,
+  color: BLACK_STROKE_COLOR,
+  backgroundColor: 'white',
 };
-
-const BLACK_BORDER = {
-  ...BORDER_STYLE,
-  borderTopColor: BORDER_COLOR,
-  borderRightColor: BORDER_COLOR,
-  borderBottomColor: BORDER_COLOR,
-  borderLeftColor: BORDER_COLOR,
-};
-
-const TRANSPARENT_BORDER = {
-  ...BORDER_STYLE,
-  borderTopColor: 'transparent',
-  borderRightColor: 'transparent',
-  borderBottomColor: 'transparent',
-  borderLeftColor: 'transparent',
+// Icon button styles -------------
+const BUTTON_STYLES = {
+  variant: 'white',
+  color: 'dark',
 };
 
 // Default theme stylings throughout entire application
 export const mantineTheme = createTheme({
   headings: {
-    sizes: {
-      h1: {
-        fontWeight: '600',
-        fontSize: '1.5rem',
-      },
-      h2: {
-        fontWeight: '600',
-        fontSize: '1.25rem',
-      },
-      h3: {
-        fontWeight: '600',
-        fontSize: '1.125rem',
-      },
-      h4: {
-        fontWeight: '400',
-        fontSize: '1rem',
-      },
-    },
+    sizes: standardTextSizes,
   },
   components: {
     ActionIcon: {
       defaultProps: {
-        variant: 'subtle',
-        color: 'dark',
+        ...BUTTON_STYLES,
         size: 'lg',
       },
       styles: {
         root: {
-          ...TRANSPARENT_BORDER,
+          '--ai-hover-color': HIGHLIGHT_STROKE_COLOR,
         },
       },
     },
@@ -65,15 +65,12 @@ export const mantineTheme = createTheme({
       },
       styles: {
         tooltip: {
-          backgroundColor: 'white',
-          ...BLACK_BORDER,
-          color: BORDER_COLOR,
+          ...TOOLTIP_STYLES,
         },
         arrow: {
-          ...BLACK_BORDER,
-          backgroundColor: 'white',
+          ...TOOLTIP_STYLES,
           filter: 'drop-shadow(0 1px 0 black)',
-        }
+        },
       },
     },
     Paper: {
@@ -84,21 +81,15 @@ export const mantineTheme = createTheme({
       },
     },
     Button: {
-      defaultProps: {
-        variant: 'outline',
-        color: 'dark',
-        style: { borderWidth: 1, fontWeight: 400 },
+      defaultProps: BUTTON_STYLES,
+      styles: {
+        root: {
+          borderColor: 'var(--button-bd)',
+          borderWidth: STROKE_WIDTH,
+          fontWeight: standardTextSizes.h4.fontWeight,
+          '--button-hover-color': HIGHLIGHT_STROKE_COLOR,
+        },
       },
-    },
-  },
-  other: {
-    iconSizes: {
-      card: 22,
-      button: 18,
-    },
-    iconStroke: {
-      base: 1,
-      card: 1.5,
     },
   },
 });
@@ -115,12 +106,14 @@ export function useThemeConstants() {
   );
 
   // Toolbar width - equal margin on both sides of 'lg' icons.
-  const toolbarWidth = 3 * Number(px(theme.spacing.lg));
+  const toolbarWidth = 3 * spacingPx.lg;
   return {
-    cardIconSize: theme.other.iconSizes.card,
-    cardIconStroke: theme.other.iconStroke.card,
-    buttonIconSize: theme.other.iconSizes.button,
-    iconStroke: theme.other.iconStroke.base,
+    // Icon sizes
+    buttonIconSize: spacingPx.md,
+    cardIconSize: spacingPx.lg,
+    // Icon stroke widths
+    cardIconStroke: DIMMED_STROKE_WIDTH,
+    iconStroke: STROKE_WIDTH,
     /**
      * Mantine theme spacing in pixels
      *
