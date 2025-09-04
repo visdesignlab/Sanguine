a3ce3c2e3ab2cf78dd582a619283f0f16b888909
Luke Schreiber <131496683+Luke-Schreiber@users.noreply.github.com>
2025-09-04 20:02:32 +0000

    Add Provider Summary & Dummy chart

--- frontend/src/Components/Views/ProvidersView/ProvidersView.tsx
+++ frontend/src/Components/Views/ProvidersView/ProvidersView.tsx
@@ -1,11 +1,13 @@
 import {
   Autocomplete,
-  Divider, Flex, Stack, Title, Tooltip,
+  Card,
+  Divider, Flex, Stack, Title, Tooltip, Text,
 } from '@mantine/core';
 import { useContext, useState } from 'react';
 import { DatePickerInput } from '@mantine/dates';
 import dayjs from 'dayjs';
 import { IconCalendarWeek, IconSearch } from '@tabler/icons-react';
+import { BarChart } from '@mantine/charts';
 import { Store } from '../../../Store/Store';
 import { useThemeConstants } from '../../../Theme/mantineTheme';
 
@@ -94,6 +96,69 @@ export function ProvidersView() {
           onChange={setDateRange}
         />
       </Flex>
+      {/* Provider Summary Card */}
+      <Card shadow="sm" radius="md" p="xl" mb="md" withBorder>
+        <Stack gap="xs">
+          <Title order={3}>Provider Summary - Dr. John Doe</Title>
+          <Title order={4} mt="xs">
+            In the past
+            {' '}
+            <Text component="span" td="underline">3 Months</Text>
+            , Dr. John Doe has recorded:
+          </Title>
+          <Stack gap={2} mt="xs">
+            <Text size="md">
+              •
+              {' '}
+              <Text component="span" td="underline">28</Text>
+              {' '}
+              Cardiac Surgeries
+            </Text>
+            <Text size="md">
+              • Used
+              {' '}
+              <Text component="span" td="underline">187</Text>
+              {' '}
+              Units of Blood Products
+            </Text>
+            <Text size="md">
+              • Average Complexity of cases
+              {' '}
+              <Text component="span" td="underline">10% higher</Text>
+              {' '}
+              than average
+            </Text>
+            <Text size="md">
+              •
+              {' '}
+              <Text component="span" td="underline">13%</Text>
+              {' '}
+              of transfused patients had post operative hemoglobin above the recommended threshold
+            </Text>
+          </Stack>
+        </Stack>
+      </Card>
+      <Title order={3}>Anemia Management</Title>
+      {/** Anemia Management Bar Chart */}
+      <Card h={150} w={300} p="md" shadow="sm">
+        <BarChart
+          h={200}
+          p="sm"
+          w="100%"
+          data={[
+            { group: 'Benchmark', Adherence: 75 },
+            { group: 'Best', Adherence: 60 },
+            { group: 'Dr. John Doe', Adherence: 25 },
+          ]}
+          dataKey="group"
+          orientation="vertical"
+          yAxisProps={{ width: 80 }}
+          barProps={{ radius: 10 }}
+          series={[{ name: 'Adherence', color: 'blue.6' }]}
+        />
+      </Card>
+      <Title order={3}>Outcomes</Title>
+
     </Stack>
   );
 }
