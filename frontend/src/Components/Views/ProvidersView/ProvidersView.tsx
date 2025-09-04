cc9cd74e4ba09e4c197da489dd9992a51306b8a9
Luke Schreiber <131496683+Luke-Schreiber@users.noreply.github.com>
2025-09-04 19:18:32 +0000

    Feat: Add dummy provider search to provider view

--- frontend/src/Components/Views/ProvidersView/ProvidersView.tsx
+++ frontend/src/Components/Views/ProvidersView/ProvidersView.tsx
@@ -1,7 +1,9 @@
 import {
+  Autocomplete,
   Divider, Flex, Stack, Title, Tooltip,
 } from '@mantine/core';
 import { useContext } from 'react';
+import { DateInput } from '@mantine/dates';
 import { Store } from '../../../Store/Store';
 import { useThemeConstants } from '../../../Theme/mantineTheme';
 
@@ -9,6 +11,7 @@ export function ProvidersView() {
   const store = useContext(Store);
 
   const { toolbarWidth } = useThemeConstants();
+  const dateSimplify = (date: Date) => date.toISOString().split('T')[0];
 
   return (
     <Stack mb="xl" gap="lg">
@@ -26,6 +29,38 @@ export function ProvidersView() {
         </Flex>
       </Flex>
       <Divider />
+      <Flex direction="row" gap="md" align="flex-end">
+        <Autocomplete
+          label="Provider"
+          placeholder="Search for a provider"
+          defaultValue="Dr. John Doe"
+          data={[
+            'Dr. John Doe',
+            'Dr. Jane Smith',
+            'Dr. Emily Carter',
+            'Dr. Michael Brown',
+          ]}
+          w="10%"
+        />
+        <DateInput
+          label="Date From"
+          value={dateSimplify(store.filtersStore.filterValues.dateFrom)}
+          onChange={(date) => date && store.filtersStore.setFilterValue('dateFrom', new Date(date))}
+          minDate={dateSimplify(store.filtersStore.initialFilterValues.dateFrom)}
+          maxDate={dateSimplify(store.filtersStore.initialFilterValues.dateTo)}
+          valueFormat="YYYY-MM-DD"
+          w="6%"
+        />
+        <DateInput
+          label="Date To"
+          value={dateSimplify(store.filtersStore.filterValues.dateTo)}
+          onChange={(date) => date && store.filtersStore.setFilterValue('dateTo', new Date(date))}
+          minDate={dateSimplify(store.filtersStore.initialFilterValues.dateFrom)}
+          maxDate={dateSimplify(store.filtersStore.initialFilterValues.dateTo)}
+          valueFormat="YYYY-MM-DD"
+          w="6%"
+        />
+      </Flex>
     </Stack>
   );
 }
