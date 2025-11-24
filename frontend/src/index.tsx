302d5efe0f79da26172a0b2176a0736996537c7b
Luke Schreiber <131496683+Luke-Schreiber@users.noreply.github.com>
2025-11-24 09:24:12 -0700

    Remove unused import in index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,6 @@ import 'mantine-datatable/styles.css';
 import '@mantine/charts/styles.css';
 import '@mantine/dates/styles.css';
 import 'react-grid-layout/css/styles.css';
-import 'react-resizable/css/styles.css';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
