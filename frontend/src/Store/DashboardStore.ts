ac53ae013ebf56dbb0b777e397147ebc7417a59f
Luke Schreiber <131496683+Luke-Schreiber@users.noreply.github.com>
2025-12-12 15:28:05 -0700

    Refactor to avoid duplicating default configs

--- frontend/src/Store/DashboardStore.ts
+++ frontend/src/Store/DashboardStore.ts
@@ -23,6 +23,54 @@ import { areLayoutsEqual, compactLayout } from '../Utils/layout';
 /**
  * DashboardStore manages the state of the PBM dashboard: stats, layouts, and chart data.
  */
+
+const DEFAULT_CHART_LAYOUTS: { [key: string]: Layout[] } = {
+  main: [
+    {
+      i: '0', x: 0, y: 0, w: 2, h: 1, maxH: 2,
+    },
+    {
+      i: '1', x: 0, y: 1, w: 1, h: 1, maxH: 2,
+    },
+    {
+      i: '2', x: 1, y: 1, w: 1, h: 1, maxH: 2,
+    },
+  ],
+};
+
+const DEFAULT_CHART_CONFIGS: DashboardChartConfig[] = [
+  {
+    chartId: '0', xAxisVar: 'month', yAxisVar: 'rbc_units', aggregation: 'sum', chartType: 'line',
+  },
+  {
+    chartId: '1', xAxisVar: 'quarter', yAxisVar: 'los', aggregation: 'avg', chartType: 'line',
+  },
+  {
+    chartId: '2', xAxisVar: 'quarter', yAxisVar: 'total_blood_product_cost', aggregation: 'sum', chartType: 'bar',
+  },
+];
+
+const DEFAULT_STAT_CONFIGS: DashboardStatConfig[] = [
+  {
+    statId: '1', yAxisVar: 'rbc_units', aggregation: 'avg', title: 'Average RBCs Transfused Per Visit',
+  },
+  {
+    statId: '2', yAxisVar: 'plt_units', aggregation: 'avg', title: 'Average Platelets Transfused Per Visit',
+  },
+  {
+    statId: '3', yAxisVar: 'cell_saver_ml', aggregation: 'sum', title: 'Total Cell Salvage Volume (ml) Used',
+  },
+  {
+    statId: '4', yAxisVar: 'total_blood_product_cost', aggregation: 'sum', title: 'Total Blood Product Costs',
+  },
+  {
+    statId: '5', yAxisVar: 'rbc_adherent', aggregation: 'avg', title: 'Guideline Adherent RBC Transfusions',
+  },
+  {
+    statId: '6', yAxisVar: 'plt_adherent', aggregation: 'avg', title: 'Guideline Adherent Platelet Transfusions',
+  },
+];
+
 export class DashboardStore {
   _rootStore: RootStore;
 
@@ -35,19 +83,8 @@ export class DashboardStore {
   // Chart settings ------------------------------------------------------------
 
   // Chart Layouts
-  _chartLayouts: { [key: string]: Layout[] } = {
-    main: [
-      {
-        i: '0', x: 0, y: 0, w: 2, h: 1, maxH: 2,
-      },
-      {
-        i: '1', x: 0, y: 1, w: 1, h: 1, maxH: 2,
-      },
-      {
-        i: '2', x: 1, y: 1, w: 1, h: 1, maxH: 2,
-      },
-    ],
-  };
+  // Chart Layouts
+  _chartLayouts: { [key: string]: Layout[] } = JSON.parse(JSON.stringify(DEFAULT_CHART_LAYOUTS));
 
   get chartLayouts() {
     return this._chartLayouts;
@@ -74,17 +111,7 @@ export class DashboardStore {
   }
 
   // Chart configurations by default
-  _chartConfigs: DashboardChartConfig[] = [
-    {
-      chartId: '0', xAxisVar: 'month', yAxisVar: 'rbc_units', aggregation: 'sum', chartType: 'line',
-    },
-    {
-      chartId: '1', xAxisVar: 'quarter', yAxisVar: 'los', aggregation: 'avg', chartType: 'line',
-    },
-    {
-      chartId: '2', xAxisVar: 'quarter', yAxisVar: 'total_blood_product_cost', aggregation: 'sum', chartType: 'bar',
-    },
-  ];
+  _chartConfigs: DashboardChartConfig[] = [...DEFAULT_CHART_CONFIGS];
 
   get chartConfigs() {
     return this._chartConfigs;
@@ -96,26 +123,7 @@ export class DashboardStore {
 
   // Stats settings ------------------------------------------------------------
   // Stat configurations by default
-  _statConfigs: DashboardStatConfig[] = [
-    {
-      statId: '1', yAxisVar: 'rbc_units', aggregation: 'avg', title: 'Average RBCs Transfused Per Visit',
-    },
-    {
-      statId: '2', yAxisVar: 'plt_units', aggregation: 'avg', title: 'Average Platelets Transfused Per Visit',
-    },
-    {
-      statId: '3', yAxisVar: 'cell_saver_ml', aggregation: 'sum', title: 'Total Cell Salvage Volume (ml) Used',
-    },
-    {
-      statId: '4', yAxisVar: 'total_blood_product_cost', aggregation: 'sum', title: 'Total Blood Product Costs',
-    },
-    {
-      statId: '5', yAxisVar: 'rbc_adherent', aggregation: 'avg', title: 'Guideline Adherent RBC Transfusions',
-    },
-    {
-      statId: '6', yAxisVar: 'plt_adherent', aggregation: 'avg', title: 'Guideline Adherent Platelet Transfusions',
-    },
-  ];
+  _statConfigs: DashboardStatConfig[] = [...DEFAULT_STAT_CONFIGS];
 
   get statConfigs() {
     return this._statConfigs;
@@ -286,52 +294,9 @@ export class DashboardStore {
    * Reset dashboard to default state
    */
   reset() {
-    this._chartLayouts = {
-      main: [
-        {
-          i: '0', x: 0, y: 0, w: 2, h: 1, maxH: 2,
-        },
-        {
-          i: '1', x: 0, y: 1, w: 1, h: 1, maxH: 2,
-        },
-        {
-          i: '2', x: 1, y: 1, w: 1, h: 1, maxH: 2,
-        },
-      ],
-    };
-
-    this._chartConfigs = [
-      {
-        chartId: '0', xAxisVar: 'month', yAxisVar: 'rbc_units', aggregation: 'sum', chartType: 'line',
-      },
-      {
-        chartId: '1', xAxisVar: 'quarter', yAxisVar: 'los', aggregation: 'avg', chartType: 'line',
-      },
-      {
-        chartId: '2', xAxisVar: 'quarter', yAxisVar: 'total_blood_product_cost', aggregation: 'sum', chartType: 'bar',
-      },
-    ];
-
-    this._statConfigs = [
-      {
-        statId: '1', yAxisVar: 'rbc_units', aggregation: 'avg', title: 'Average RBCs Transfused Per Visit',
-      },
-      {
-        statId: '2', yAxisVar: 'plt_units', aggregation: 'avg', title: 'Average Platelets Transfused Per Visit',
-      },
-      {
-        statId: '3', yAxisVar: 'cell_saver_ml', aggregation: 'sum', title: 'Total Cell Salvage Volume (ml) Used',
-      },
-      {
-        statId: '4', yAxisVar: 'total_blood_product_cost', aggregation: 'sum', title: 'Total Blood Product Costs',
-      },
-      {
-        statId: '5', yAxisVar: 'rbc_adherent', aggregation: 'avg', title: 'Guideline Adherent RBC Transfusions',
-      },
-      {
-        statId: '6', yAxisVar: 'plt_adherent', aggregation: 'avg', title: 'Guideline Adherent Platelet Transfusions',
-      },
-    ];
+    this._chartLayouts = JSON.parse(JSON.stringify(DEFAULT_CHART_LAYOUTS));
+    this._chartConfigs = [...DEFAULT_CHART_CONFIGS];
+    this._statConfigs = [...DEFAULT_STAT_CONFIGS];
 
     this.computeChartData();
     this.computeStatData();
