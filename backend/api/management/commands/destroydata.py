98c34ab48e21a5655b5ae96d28b7728d9d14ae8a
Luke Schreiber <131496683+Luke-Schreiber@users.noreply.github.com>
2026-02-27 09:16:34 -0700

    Add 'with' check to destroydata

--- backend/api/management/commands/destroydata.py
+++ backend/api/management/commands/destroydata.py
@@ -9,9 +9,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write("Deleting all data from the database...")
 
-        with transaction.atomic():
-            for model in apps.get_models():
-                if model._meta.app_label == "api":
-                    model.objects.all().delete()
-                    self.stdout.write(f"Deleted all data from {model.__name__}")
+        with connection.constraint_checks_disabled():
+            with transaction.atomic():
+                for model in apps.get_models():
+                    if model._meta.app_label == "api":
+                        model.objects.all().delete()
+                        self.stdout.write(f"Deleted all data from {model.__name__}")
         self.stdout.write(self.style.SUCCESS("Successfully deleted all data from the database."))
